28fe0e0e82bbf9fc5abe1a3c0e63be286fcb5413
dovjas <dovydas.jaseliunas@gmail.com>
2022-05-23 14:54:59 +0300

    Second commit

--- main.py
+++ main.py
@@ -7,28 +7,29 @@ MR_M = 3.00
 MR_L = 4.00
 
 # Lists of data: Date,Size and Carrier
-input_data = []
+input_data_raw = []
 date_raw = []
 size_raw = []
 carrier_raw = []
 
 # List of cost per delivery
 cost = []
+
 # List of discount per delivery
 discount = []
-
 # Count  L shipment via LP
 count_LP_L = 0
+
 # The third L shipment via LP is free, once a calendar month.
 calendar_days = 30
 
 # Open and read input.txt data
 with open('input.txt','r+') as f: lines = f.read().splitlines()
 for line in lines:
-    input_data.append(line.split(" "))
+    input_data_raw.append(line.split(" "))
 
 # Select specific data in file and create lists of: Date,Size and Carrier
-for data in input_data:
+for data in input_data_raw:
     date_raw.append((data[0]))
     size_raw.append(data[1])
     try:
@@ -36,8 +37,8 @@ for data in input_data:
     except:
         carrier_raw.append("Ignore")
 
-# Calculation module
-for value in input_data:
+ # Calculation module
+for value in input_data_raw:
     try:
         if value[1] == 'S' and value[2] == 'LP':
             price = LP_MR_S
@@ -82,14 +83,20 @@ for value in input_data:
             price = 'Ignored'
             cost.append(price)
 
-print(cost)
-print(count_LP_L )
-print(discount)
 
-discount_sum = 0
-# for x in discount:
-#     try:
-#         discount_sum = discount_sum + float(x)
-#     except:
-#         pass
+# Make a flat list (out of a list of lists)
+input_data = [item for sublist in input_data_raw for item in sublist]
+
+all_data = []
+all_data.append(input_data)
+all_data.append(cost)
+all_data.append(discount)
+all_data = [item for sublist in all_data for item in sublist]
+
+with open("result.txt", "w") as file:
+    content = str(all_data)
+    file.write(content)
+    file.close()
+
+
 
